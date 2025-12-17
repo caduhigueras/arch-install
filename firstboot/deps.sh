92ccbae373ae56174edb94637cb21319d53e2bf7
Cadu Higueras <cadu.higueras@gmail.com>
2025-12-17 20:24:37 +0000

    Updates

--- firstboot/deps.sh
+++ firstboot/deps.sh
@@ -11,4 +11,4 @@ cd ~/.local/share/yay
 makepkg -si --noconfirm --needed
 
 # cronie 
-sudo pacman -S --noconfirm --needed bat blueman bluez bluez-utils cairo dunst evince feh gum make nautilus nwg-look poppler-glib protobuf pv reflector ripgrep rsync satty sof-firmware stow  uwsm waybar wl-clipboard 
+sudo pacman -S --noconfirm --needed 7zip bat blueman bluez bluez-utils cairo dunst evince feh ffmpeg gum lsof make nautilus nwg-look openssh poppler-glib protobuf pv reflector ripgrep rsync satty sof-firmware stow tar uwsm waybar wl-clipboard zip
