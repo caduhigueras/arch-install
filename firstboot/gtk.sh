3bcb22e8675d36a10f6a843c9bf21fd9b6691a04
Carlos Dias <c.dias@onedirect.com>
2026-01-07 15:16:05 +0100

    Remove murrine for now

--- firstboot/gtk.sh
+++ firstboot/gtk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo pacman -S --needed --noconfirm gtk3 gtk4 gtk-layer-shell gtk4-layer-shell
-yay -S --needed --noconfirm gtk-engine-murrine
+# yay -S --needed --noconfirm gtk-engine-murrine
 
 git clone https://github.com/Fausto-Korpsvart/Tokyonight-GTK-Theme /home/arch/Downloads/gtk-tokyonight
 cd /home/arch/Downloads/gtk-tokyonight/themes
