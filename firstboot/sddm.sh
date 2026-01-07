f728fc978a58bd2fcfb0722b3f2b87a4f3a59f37
Cadu Higueras <cadu.higueras@gmail.com>
2026-01-07 21:17:05 +0000

    fix typos

--- firstboot/sddm.sh
+++ firstboot/sddm.sh
@@ -4,8 +4,8 @@ sudo pacman -S --needed --noconfirm sddm sddm-kcm
 
 cd /home/arch/.local/share/arch-dotfiles
 
-sudo cp sddm/sdd.conf /etc/sddm.conf
+sudo cp sddm/sddm.conf /etc/sddm.conf
 sudo cp -r sddm/kr_minimal /usr/share/sddm/themes/
 sudo chmod -R a+rX /usr/share/sddm/themes/kr_minimal
 
-cd /root/arch-installer
+cd /opt/arch-installer
