7f72328890a7a11f7ef47f046156c92e86a92cbe
Cadu Higueras <cadu.higueras@gmail.com>
2026-01-07 16:40:21 +0000

    general updates

--- firstboot/hyprland.sh
+++ firstboot/hyprland.sh
@@ -4,9 +4,3 @@ sudo pacman -S --noconfirm --needed xdg-desktop-portal-hyprland hyprpolkitagent 
 sudo pacman -S --noconfirm --needed hyprland xorg
 yay -S --needed --noconfirm hyprcap
 
-# hyprpm update
-# yes | hyprpm add https://github.com/levnikmyskin/hyprland-virtual-desktops  && hyprpm enable virtual-desktops
-# TODO: move to dotfiles
-# exec-once = bash -lc 'test -f ~/.cache/hyprpm-vd-installed || (hyprpm update && hyprpm add https://github.com/levnikmyskin/hyprland-virtual-desktops && touch ~/.cache/hyprpm-vd-installed)'
-# exec-once = hyprpm enable virtual-desktops || true
-
