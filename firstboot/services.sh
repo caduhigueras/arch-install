1fcb73b9df359e74bb7f720ae9957ad51b2c75c9
Cadu Higueras <cadu.higueras@gmail.com>
2026-01-08 07:59:29 +0000

    remove rm

--- firstboot/services.sh
+++ firstboot/services.sh
@@ -4,5 +4,3 @@ sudo systemctl enable sddm
 sudo systemctl enable bluetooth
 sudo systemctl enable cronie.service
 sudo systemctl start cronie.service
-
-sudo rm "/etc/sudoers.d/99-$USERNAME-nopasswd"
