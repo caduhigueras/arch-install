e1e790419f533884fa4a9d8fc95cb517f2f2f0d2
Cadu Higueras <cadu.higueras@gmail.com>
2026-01-10 13:23:12 +0100

    Adding teams scripts for notifications

--- firstboot/services.sh
+++ firstboot/services.sh
@@ -9,4 +9,30 @@ sudo systemctl disable firstboot-setup
 
 sudo rm -rf /etc/sudoers.d/99*
 
+cat > /etc/systemd/system/teams-notifications.service << "EOF"
+[Unit]
+Description=Teams notification listener
+
+[Service]
+ExecStart=teams-notification-listener
+Restart=always
+
+[Install]
+WantedBy=default.target
+EOF
+systemctl enable teams-focus-reset.service
+
+cat > /etc/systemd/system/teams-notifications.service << "EOF"
+[Unit]
+Description=Reset Teams unread counter on focus
+
+[Service]
+ExecStart=teams-reset-count-on-focus
+Restart=always
+
+[Install]
+WantedBy=default.target
+EOF
+systemctl enable teams-focus-reset.service
+
 sudo reboot now
