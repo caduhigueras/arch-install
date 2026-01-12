db73d1157c0181edea592999769677721cf880b5
Carlos Dias <c.dias@onedirect.com>
2026-01-12 10:45:13 +0100

    update path

--- firstboot/bashrc.sh
+++ firstboot/bashrc.sh
@@ -62,7 +62,7 @@ showmycmds() {
     echo "cdi => cd $HOME/app/dev/arch-install/"
     echo "cdm => cd $HOME/app/magento/"
     echo "cdm2 => docker exec -it php-fpm bash"
-    echo "cdn => cd $HOME/.config/nvim"
+    echo "cdn => cd $HOME/.local/share/nvim"
     echo "cdo => cd $HOME/.local/share/obsidian"
     echo "cds =>cd $HOME/app/sites"
     echo "cdsc => cd $HOME/files/scratches/"
