23721b2429c982ca7ba1aa7b38d94f8d6ebc3ae8
Carlos Dias <c.dias@onedirect.com>
2026-01-14 11:33:57 +0100

    Ypdate theme sh

--- firstboot/themes.sh
+++ firstboot/themes.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir -p "$HOME/.config/themes/current/"
-ln -s "$HOME/.config/themes/tokyo-night/wallpapers/" "$HOME/.config/themes/current/wallpapers/"
+mkdir -p "$HOME/.config/themes/"
+ln -s "$HOME/.config/themes/tokyo-night/" "$HOME/.config/themes/current/"
 ln -s "$HOME/.config/themes/tokyo-night/wallpapers/wallpaper-1.jpg" "$HOME/.config/themes/current/wallpaper.jpg"
