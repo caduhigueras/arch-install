74bec95a195a6ee132fb3a70266851e9586751cf
Carlos Dias <c.dias@onedirect.com>
2026-01-14 13:25:29 +0100

    Fix service error

--- firstboot/services.sh
+++ firstboot/services.sh
@@ -5,34 +5,34 @@ sudo systemctl enable bluetooth
 sudo systemctl enable cronie.service
 sudo systemctl start cronie.service
 sudo systemctl disable firstboot-setup
-# sudo systemctl --user start elephant
 
-# sudo rm -rf /etc/sudoers.d/99*
-
-sudo cat > /etc/systemd/system/teams-notifications.service << "EOF"
+cat <<'EOF' | sudo tee /etc/systemd/system/teams-notifications.service >/dev/null
 [Unit]
 Description=Teams notification listener
 
 [Service]
-ExecStart=teams-notification-listener
+ExecStart=/usr/local/bin/teams-notification-listener
 Restart=always
 
 [Install]
-WantedBy=default.target
+WantedBy=multi-user.target
 EOF
-sudo systemctl enable teams-focus-reset.service
 
-sudo cat > /etc/systemd/system/teams-focus-reset.service << "EOF"
+cat <<'EOF' | sudo tee /etc/systemd/system/teams-focus-reset.service >/dev/null
 [Unit]
 Description=Reset Teams unread counter on focus
 
 [Service]
-ExecStart=teams-reset-count-on-focus
+ExecStart=/usr/local/bin/teams-reset-count-on-focus
 Restart=always
 
 [Install]
-WantedBy=default.target
+WantedBy=multi-user.target
 EOF
-sudo systemctl enable teams-focus-reset.service
 
+sudo systemctl daemon-reload
+sudo systemctl enable --now teams-notifications.service
+sudo systemctl enable --now teams-focus-reset.service
+
+sudo rm -rf /etc/sudoers.d/99*
 sudo reboot now
