a201bc9072fb8b0066fc0d69141b40acaca69ea4
Carlos Dias <c.dias@onedirect.com>
2026-01-14 11:33:08 +0100

    Update wallpaper extract

--- firstboot/dotfiles.sh
+++ firstboot/dotfiles.sh
@@ -2,7 +2,11 @@
 
 git clone https://github.com/caduhigueras/arch-dotfiles.git ~/.local/share/arch-dotfiles/
 
-cd ~/.local/share/arch-dotfiles/
+cd "$HOME/.local/share/arch-dotfiles/config/.config/themes/tokyo-night/wallpapers"
+unzip wallpapers.zip
+rm wallpapers.zip
+
+cd "$HOME/.local/share/arch-dotfiles/"
 
 rm -rf ~/.config/hypr
 
