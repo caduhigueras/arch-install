382442277c6bfa6e685a79ded68018516d52ea2b
Cadu Higueras <cadu.higueras@gmail.com>
2026-01-29 14:19:50 +0000

    Teams services moved to dofiles

--- firstboot/services.sh
+++ firstboot/services.sh
@@ -5,34 +5,7 @@ sudo systemctl enable bluetooth
 sudo systemctl enable cronie.service
 sudo systemctl start cronie.service
 sudo systemctl disable firstboot-setup
-
-cat <<'EOF' | sudo tee /etc/systemd/system/teams-notifications.service >/dev/null
-[Unit]
-Description=Teams notification listener
-
-[Service]
-ExecStart=/usr/local/bin/teams-notification-listener
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-cat <<'EOF' | sudo tee /etc/systemd/system/teams-focus-reset.service >/dev/null
-[Unit]
-Description=Reset Teams unread counter on focus
-
-[Service]
-ExecStart=/usr/local/bin/teams-reset-count-on-focus
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
 sudo systemctl daemon-reload
-sudo systemctl enable --now teams-notifications.service
-sudo systemctl enable --now teams-focus-reset.service
 
 #sudo sh -c "sudo rm -rf /etc/sudoers.d/99-$USER-nopasswd && reboot now"
 sudo reboot now
